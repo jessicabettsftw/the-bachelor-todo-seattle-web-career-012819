60b35b180ca0e0a99f3ca762942a3ce546028bc6
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-17 04:46:27 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,27 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |this_season, info|
+    if this_season == season
+      info.each do |contestant|
+        if contestant["status"] == "Winner"
+          name = contestant["name"]
+          name_array = name.split(" ")
+          return name_array[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |this_season, info|
+    if this_season == season
+      info.each do |contestant|
+        if contestant["occupation"] == occupation
+          return contestant["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)

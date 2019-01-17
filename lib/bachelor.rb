fb463974a67db89faa59fb34a6688611262e47b8
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-17 04:55:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -50,7 +50,7 @@ def get_average_age_for_season(data, season)
   data.each do |this_season, info|
     if this_season == season
       info.each do |contestant|
-        if contestant[]
+        age = contestant[]
       end
     end
   end

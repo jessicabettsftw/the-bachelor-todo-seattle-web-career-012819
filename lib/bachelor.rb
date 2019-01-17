7a90279e9900ee0dd56c163be60370bead1b41bf
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-17 05:01:07 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,61 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |this_season, info|
+    if this_season == season
+      info.each do |contestant|
+        if contestant["status"] == "Winner"
+          name = contestant["name"]
+          name_array = name.split(" ")
+          return name_array[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |this_season, info|
+      info.each do |contestant|
+        if contestant["occupation"] == occupation
+          return contestant["name"]
+        end
+      end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |this_season, info|
+      info.each do |contestant|
+        if contestant["hometown"] == hometown
+          count += 1
+        end
+      end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |this_season, info|
+      info.each do |contestant|
+        if contestant["hometown"] == hometown
+          return contestant["occupation"]
+        end
+      end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  average_age = 0
+  number_contestants = 0
+  
+  data.each do |this_season, info|
+    if this_season == season
+      info.each do |contestant|
+        average_age += contestant["age"].to_f
+        number_contestants += 1
+      end
+    end
+  end
+  
+  return (average_age / number_contestants).round
 end

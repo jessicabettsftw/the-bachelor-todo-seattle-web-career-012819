9cb3b229d35b121ae62d3a8968a482700a901e7e
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-17 04:51:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -35,7 +35,17 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |this_season, info|
+    if this_season == season
+      info.each do |contestant|
+        if contestant["status"] == "Winner"
+          name = contestant["name"]
+          name_array = name.split(" ")
+          return name_array[0]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)

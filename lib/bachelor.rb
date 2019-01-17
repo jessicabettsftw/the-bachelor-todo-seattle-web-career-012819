54bcb581df38d77d3d1b9093b291537f45db0847
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-17 04:50:22 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -26,7 +26,7 @@ def count_contestants_by_hometown(data, hometown)
   data.each do |this_season, info|
       info.each do |contestant|
         if contestant["hometown"] == hometown
-          return contestant["name"]
+          count += 1
         end
       end
   end
